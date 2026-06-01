5720afdb7fcc1b9e2de87d041a747fe29559a043
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-06-01 11:13:42 +0200

    first commit of Shirinyota company ltd

--- frontend/src/components/stock_in/MonthlyReport.jsx
+++ frontend/src/components/stock_in/MonthlyReport.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const MonthlyReport = () => {
     const [stockInReport, setStockInReport] = useState(null);
     const [stockOutReport, setStockOutReport] = useState(null);
-    
+    const [totals, setTotals] = useState(null);
 
     const handleGetStockOutReport = async () => {
         try {
@@ -25,8 +25,18 @@ const MonthlyReport = () => {
         }
     }
 
+    const handleGetStockTotals = async () => {
+        try {
+           const res = await axios.get('http://localhost:3000/stockIn/report/totals');
+           setTotals(res.data.totals);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     useEffect(() => {
         handleGetStockInReport();
+        handleGetStockTotals();
     }, []); 
   
     useEffect(() => {
@@ -35,6 +45,35 @@ const MonthlyReport = () => {
 
     return (
         <div className="bg-blue-50 min-h-screen">
+                     <div className="bg-blue-200 fixed top-0 left-0 right-0">
+                        <h1 className="text-2xl ms-3 text-wrap font-bold text-amber-500">Reports</h1>
+                        <div className="mt-3 ms-4">
+                             <h1 className="text-xl font-bold text-amber-500">Quck Data</h1>
+                             <div className="grid grid-cols-4 space-x-4 mt-3">
+                                <div className="bg-green-300 p-2 py-4 rounded-lg">
+                                    <p className="text-center text-gray-700">Total Stock In Value</p>
+                                    <p className="text-center font-bold">FRW: {totals?.totalStockInAmouth}</p>
+                                </div>
+                                <div className="bg-amber-300 p-2 py-4 rounded-lg">
+                                    <h1 className="text-center text-gray-700">Total Stock In Quantity</h1>
+                                    <p className="text-center font-bold">{totals?.totalStockInQuantity}</p>
+                                </div>
+                                <div className="bg-blue-300 p-2 py-4 rounded-lg">
+                                    <h1  className="text-center text-gray-700">Total Stock Out Quantity</h1>
+                                    <p className="text-center font-bold">{totals?.totalStockOutQuantity}</p>
+                                </div>
+                                <div className="bg-cyan-300 p-2 py-4 rounded-lg me-3">
+                                    <h1  className="text-center text-gray-700">Total Remaining stock</h1>
+                                    <p className="text-center font-bold">{totals?.remainingStock} </p> 
+                                </div>
+                             </div>
+                        </div>
+                        <div className="mt-4 flex space-x-6 ms-4">
+                            <Link className="bg-green-300 py-2 px-6 text-gray-500 font-bold rounded-lg" to={'/report/daily'}>Daily</Link>
+                            <Link className="bg-blue-300 py-2 px-6 text-gray-500 font-bold rounded-lg" to={'/report/weekly'}>Weekly</Link>
+                            <Link className="bg-amber-300 py-2 px-6 text-gray-500 font-bold rounded-lg" to={'/report/monthly'}>Monthly</Link>
+                        </div>
+                    </div>
             <div className="max-w-7xl mx-auto w-full  mt-60">
                 <h1 className="text-xl text-center font-bold text-blue-500">Stock Out Monthly Report</h1>
                 <table className="w-full">
