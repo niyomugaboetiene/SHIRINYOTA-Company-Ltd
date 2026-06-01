0b2cacbe4494d327b19309f91893bd9c7230c8bd
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-06-01 10:29:57 +0200

    first commit of Shirinyota company ltd

--- frontend/src/components/stock_in/DailyReport.jsx
+++ frontend/src/components/stock_in/DailyReport.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const Report = () => {
     const [stockInReport, setStockInReport] = useState(null);
-    const [stockOutRoute, setStockOutReport] = useState(null);
+    const [stockOutReport, setStockOutReport] = useState(null);
     
 
     const handleGetStockOutReport = async () => {
@@ -35,12 +35,29 @@ const Report = () => {
     return (
         <div>
             <div>
+                <h1>Stock Out Daily Report</h1>
                 <table>
                     <thead>
                         <tr>
-                            <th></th>
+                            <th>Product Id</th>
+                            <th>Product Name</th>
+                            <th>Date</th>
+                            <th>Quantity</th>
+                            <th>Product Id</th>
+                            <th>Product Id</th>
                         </tr>
                     </thead>
+
+                    <tbody>
+                        {stockOutReport?.map((product, index) => (
+                            <tr>
+                                <td>{product.Product_Id?.Product_Id}</td>
+                                <td>{product.Product_Id?.Product_Name}</td>
+                                <td>{new Date(product.Date).toLocaleDateString()}</td>
+                                <td>{product.Quantity}</td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
             </div>
         </div>
