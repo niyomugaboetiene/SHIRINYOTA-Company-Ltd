f0f767f6fc4a5fa5b920de9a45ab74fd6474450b
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-06-01 06:34:43 +0200

    first commit of Shirinyota company ltd

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -6,6 +6,10 @@ const router = express.Router();
 router.post('/addNew', async (req, res) => {
     try {
         // Product_Id(PK), Product_Name,
-        
+        const { Product_Id, Product_Name } = req.body;
+
+        if (!Product_Id || !Product_Name) {
+            return res.status()
+        }
     }
 })
