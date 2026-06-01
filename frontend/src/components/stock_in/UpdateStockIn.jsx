2e759d98d08ba53806c0b447a60154c9cf0b8d15
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-06-01 11:41:44 +0200

    first commit of Shirinyota company ltd

---
+++ frontend/src/components/stock_in/UpdateStockIn.jsx
@@ -0,0 +1,87 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+const AddStockIn = () => {
+    // Product_Id(FK),Date,Quantity,Unit_Price,Total_Price
+    const [Product_Id, setProduct_Id] = useState("");
+    const [Date, setDate] = useState("");
+    const [Quantity, setQuantity] = useState(0);
+    const [Unit_Price, setUnit_Price] = useState(0);
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState(""); 
+    const { _id } = useParams();
+    const [product, setProduct] = useState(null);
+
+    const handleUpdateStockIn = async () => {
+        try {
+            const res = await axios.put(`http://localhost:3000/stockIn/update//${_id}`, { Product_Id, Date, Quantity, Unit_Price });
+           setMessage(res.data.message);
+        } catch (err) {
+            console.error(err);
+            setError(err?.response?.data?.message || "Error occured");
+        }
+    }
+
+    const handleGetProduct = async () => {
+        try {
+            const res = await axios.get('http://localhost:3000/product/list');
+            setProduct(res.data.list);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        handleGetProduct();
+    }, []);
+    return (
+        <div className="min-h-screen bg-blue-100 flex justify-center items-center">
+            <div className="bg-white p-4 rounded-lg w-1/5">
+           <h1 className="text-center text-xl text-blue-500 font-bold">Update Stock In Portal</h1>
+           {message && (
+            <p>{message}</p>
+           )}
+           {error && (
+            <p>{error}</p>
+           )}
+            <div className="mt-2">
+                 <label className="block text-blue-500">Product</label>
+                 <select onChange={(e) => setProduct_Id(e.target.value)} className="bg-gray-300 w-full py-3 px-3 rounded-full text-white font-bold focus:outline-2 focus:outline-blue-200">
+                    {product?.map((prod, index) => (
+                        <option value={prod._id} key={index}>{prod.Product_Name}</option>
+                    ))}
+                 </select>
+            </div>
+            <div className="mt-2">
+                <label htmlFor="" className="text-blue-500 block">Date</label>
+                <input type="date" 
+                className="bg-gray-300 w-full py-3 px-3 rounded-full text-white font-bold focus:outline-2 focus:outline-blue-200"
+                onChange={(e) => setDate(e.target.value)} />
+            </div>
+            <div className="mt-2">
+                <label htmlFor="" className="text-blue-500 block">Quantity</label>
+                <input type="number" 
+                className="bg-gray-300 w-full py-3 px-3 rounded-full text-white font-bold focus:outline-2 focus:outline-blue-200"
+                onChange={(e) => setQuantity(e.target.value)} />
+            </div>
+            <div className="mt-2">
+                <label htmlFor="" className="text-blue-500 block">Unit Price</label>
+                <input type="number" 
+                className="bg-gray-300 w-full py-3 px-3 rounded-full text-white font-bold focus:outline-2 focus:outline-blue-200"
+                onChange={(e) => setUnit_Price(e.target.value)} />
+            </div>
+
+            <button
+              className="mt-4 bg-blue-300 w-full py-3 px-3 rounded-full text-white font-bold hover:bg-blue-400 transition-colors" 
+              onClick={handleUpdateStockIn}
+            >
+                Save
+            </button>
+        </div>
+        </div>
+    )
+}
+
+export default AddStockIn;
