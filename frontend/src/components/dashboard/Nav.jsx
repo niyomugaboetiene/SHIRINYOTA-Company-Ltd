4012bbffbfa20296742af3655c4f592b8fdf912f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-06-01 19:04:09 +0200

    first commit of Shirinyota company ltd

--- frontend/src/components/dashboard/Nav.jsx
+++ frontend/src/components/dashboard/Nav.jsx
@@ -5,12 +5,18 @@ const NavBar = () => {
         <div>
             <div>
                 <div>
+                    <h1>SHIRINYOTA</h1>
+                </div>
+                <div>
                     <nav>
                         <Link>Home</Link>
                         <Link>Products</Link>
                         <Link>Stock In</Link>
                         <Link>Stock Out</Link>
                     </nav>
+                </div>
+                <div>
+                    Admin Data
                 </div>
             </div>
         </div>
