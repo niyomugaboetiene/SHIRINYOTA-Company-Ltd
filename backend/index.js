42b91c2dd2942960bae8a009229a400c7a39f7f0
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-06-01 09:10:26 +0200

    first commit of Shirinyota company ltd

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,15 @@ import StockInRoute from "./routes/Stock_InRoute.js";
 import StockOutRoute from "./routes/Stock_OutRoute.js";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/product', ProductRoute);
+app.use('/stockIn', StockInRoute);
+app.use('/stockOut', StockOutRoute);
+
+
+app.listen(5000, () => {
+    console.log("http://localhost:5000");
+});
