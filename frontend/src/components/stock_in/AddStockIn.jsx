77ff521bc23fe8730b8b39b80bd337a521c821b6
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-06-01 11:17:56 +0200

    first commit of Shirinyota company ltd

---
+++ frontend/src/components/stock_in/AddStockIn.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+
+const HandleAddStockIn = () => {
+    // Product_Id(FK),Date,Quantity,Unit_Price,Total_Price
+    const [Product_Id, setProduct_Id] = useState("");
+    const [Date, setDate] = useState("");
+    const [Quantity, setQuantity] = useState(0);
+    const [Unit_Price, setUnit_Price] = useState(0);
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState(""); 
+
+    const handleAddStockIn = async () => {
+        try {
+            const res = await axios.post('http://localhost:3000/stockIn/addNew', { Product_Id, Date, Quantity, Unit_Price });
+           setMessage(res.data.message);
+        } catch (err) {
+            console.error(err);
+            setError(err?.response?.data || "Error occured");
+        }
+    }
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
